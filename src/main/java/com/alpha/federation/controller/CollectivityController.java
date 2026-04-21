cb9ecf14d4abcc2ac316b34f9b9eccf1aefc1ab0
mekill404 <hei.mahery.55@gmail.com>
2026-04-21 17:00:00 +0300

    feat: update controllers to handle collectivity creation and member management

--- src/main/java/com/alpha/federation/controller/CollectivityController.java
+++ src/main/java/com/alpha/federation/controller/CollectivityController.java
@@ -1,13 +1,13 @@
 package com.alpha.federation.controller;
 
-
-
 import com.alpha.federation.model.Collectivity;
 import com.alpha.federation.service.CollectivityService;
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/collectivities")
@@ -20,14 +20,14 @@ public class CollectivityController {
     }
 
     @PostMapping
-    public ResponseEntity<Collectivity> create(@RequestBody Collectivity collectivity) {
+    public ResponseEntity<List<Collectivity>> createCollectivities(@RequestBody List<Collectivity> requests) {
         try {
-            Collectivity created = collectivityService.createCollectivity(collectivity);
-
-            return new ResponseEntity<>(created, HttpStatus.CREATED);
+            List<Collectivity> created = collectivityService.createCollectivities(requests);
+            return ResponseEntity.status(HttpStatus.CREATED).body(created);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         } catch (RuntimeException e) {
-
-            return new ResponseEntity<>((HttpHeaders) null, HttpStatus.BAD_REQUEST);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 }
--- src/main/java/com/alpha/federation/controller/MemberController.java
+++ src/main/java/com/alpha/federation/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.alpha.federation.controller;
 
 import com.alpha.federation.model.Member;
 import com.alpha.federation.service.MemberService;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,10 +22,12 @@ public class MemberController {
     @PostMapping
     public ResponseEntity<List<Member>> createMembers(@RequestBody List<Member> members) {
         try {
-            List<Member> createdMembers = memberService.registerMembers(members);
-            return ResponseEntity.status(HttpStatus.CREATED).body(createdMembers);
-        } catch (RuntimeException e) {
+            List<Member> created = memberService.registerMembers(members);
+            return ResponseEntity.status(HttpStatus.CREATED).body(created);
+        } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 }
