0712e79a6abe1fc892abf7648dfe5b992fe640a1
mekill404 <hei.mahery.55@gmail.com>
2026-04-21 17:30:00 +0300

    refactor(model): migration de Collectivity vers CollectivityEntity

---
+++ src/main/java/com/alpha/federation/model/CollectivityEntity.java
@@ -0,0 +1,23 @@
+package com.alpha.federation.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class CollectivityEntity {
+    private String id;
+    private String location;
+    private boolean federationApproval;
+    private LocalDate approvalDate;
+    private MemberEntity president;
+    private MemberEntity vicePresident;
+    private MemberEntity treasurer;
+    private MemberEntity secretary;
+    private List<MemberEntity> members = new ArrayList<>();
+}
