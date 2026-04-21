b341c019886a4e1d41357d1561bf387e0f18d90c
mekill404 <hei.mahery.55@gmail.com>
2026-04-21 17:40:00 +0300

    refactor(model): migration de Member vers MemberEntity

--- src/main/java/com/alpha/federation/model/MemberEntity.java
+++ src/main/java/com/alpha/federation/model/MemberEntity.java
@@ -1,24 +1,19 @@
 package com.alpha.federation.model;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
 import com.alpha.federation.model.enums.Gender;
 import com.alpha.federation.model.enums.Occupation;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
-public class Member {
-
+public class MemberEntity {
     private String id;
-
     private String firstName;
     private String lastName;
     private LocalDate birthDate;
@@ -28,22 +23,6 @@ public class Member {
     private String phoneNumber;
     private String email;
     private Occupation occupation;
-
-    private List<Member> referees = new ArrayList<>();
-
-    @JsonIgnore
-    private List<String> refereeIds;
-
-    @JsonIgnore
-    private String collectivityIdentifier;
-
-    @JsonIgnore
-    private boolean registrationFeePaid;
-
-    @JsonIgnore
-    private boolean membershipDuesPaid;
-
-    public void setReferees(List<String> refereeIds) {
-        this.refereeIds = refereeIds;
-    }
+    private LocalDate createdAt;
+    private List<MemberEntity> referees = new ArrayList<>();
 }
