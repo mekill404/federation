6e562bcf9817610942347d585bd0cf5248274c7c
mekill404 <hei.mahery.55@gmail.com>
2026-04-21 19:50:00 +0300

    refactor(controller): mise à jour des endpoints de Collectivity

--- src/main/java/com/alpha/federation/controller/CollectivityController.java
+++ src/main/java/com/alpha/federation/controller/CollectivityController.java
@@ -1,12 +1,10 @@
 package com.alpha.federation.controller;
 
-import com.alpha.federation.model.Collectivity;
+import com.alpha.federation.dto.request.CreateCollectivityRequest;
+import com.alpha.federation.dto.response.CollectivityResponse;
 import com.alpha.federation.service.CollectivityService;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -20,14 +18,8 @@ public class CollectivityController {
     }
 
     @PostMapping
-    public ResponseEntity<List<Collectivity>> createCollectivities(@RequestBody List<Collectivity> requests) {
-        try {
-            List<Collectivity> created = collectivityService.createCollectivities(requests);
-            return ResponseEntity.status(HttpStatus.CREATED).body(created);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<CollectivityResponse> createCollectivities(@RequestBody List<CreateCollectivityRequest> requests) {
+        return collectivityService.createCollectivities(requests);
     }
 }
